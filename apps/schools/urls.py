a346b33933de8e2d839b178b821919ca3d443149
cloudartisan <david@cloudartisan.com>
2016-09-24 22:27:50 +1000

    Adds stub URLs for the various views

--- apps/schools/urls.py
+++ apps/schools/urls.py
@@ -1,5 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 import views
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^$', views.ListSchoolView.as_view(), name='schools-list',),
+    url(r'^(?P<school_pk>[\d]+)/$', views.DetailSchoolView.as_view(), name='schools-detail',),
+    url(r'^(?P<school_pk>[\d]+)/edit$', views.UpdateSchoolView.as_view(), name='schools-update',),
+    url(r'^add/$', views.CreateSchoolView.as_view(), name='schools-create',),
+    url(r'^(?P<school_pk>[\d]+)/delete$', views.DeleteSchoolView.as_view(), name='schools-delete',),
+]
