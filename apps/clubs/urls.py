4d79dd08f4441780cbafcf9f1f10daa71fb50df9
cloudartisan <david@cloudartisan.com>
2016-09-24 22:22:13 +1000

    Adds stub club URLs

--- apps/clubs/urls.py
+++ apps/clubs/urls.py
@@ -1,13 +1,25 @@
+"""
 from django.conf.urls import include, url
 
 import views
 
-#from organizations.backends import invitation_backend
-#from organizations.urls import urlpatterns
+from organizations.backends import invitation_backend
+from organizations.urls import urlpatterns
 
 # Extends organizations.urls.urlpatterns
-#urlpatterns += [
-    #url(r'^invitations/', include(invitation_backend().get_urls())),
-#]
+urlpatterns += [
+    url(r'^invitations/', include(invitation_backend().get_urls())),
+]
+"""
 
-urlpatterns = []
+from django.conf.urls import url
+
+import views
+
+urlpatterns = [
+    url(r'^$', views.ListClubView.as_view(), name='clubs-list',),
+    url(r'^(?P<club_pk>[\d]+)/$', views.DetailClubView.as_view(), name='clubs-detail',),
+    url(r'^(?P<club_pk>[\d]+)/edit$', views.UpdateClubView.as_view(), name='clubs-update',),
+    url(r'^add/$', views.CreateClubView.as_view(), name='clubs-create',),
+    url(r'^(?P<club_pk>[\d]+)/delete$', views.DeleteClubView.as_view(), name='clubs-delete',),
+]
