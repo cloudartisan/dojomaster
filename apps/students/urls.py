bd2f315b5c456b20601e910c144907b2de58228b
cloudartisan <david@cloudartisan.com>
2016-09-25 23:28:35 +1000

    Clean up

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -4,8 +4,8 @@ import views
 
 urlpatterns = [
     url(r'^$', views.ListStudentView.as_view(), name='students-list',),
-    url(r'^(?P<student_pk>[\d]+)/$', views.DetailStudentView.as_view(), name='students-detail',),
-    url(r'^(?P<student_pk>[\d]+)/edit$', views.UpdateStudentView.as_view(), name='students-update',),
-    url(r'^add/$', views.CreateStudentView.as_view(), name='students-create',),
-    url(r'^(?P<student_pk>[\d]+)/delete$', views.DeleteStudentView.as_view(), name='students-delete',),
+    url(r'^(?P<student_pk>[\d]+)/$', views.DetailStudentView.as_view(), name='student-detail',),
+    url(r'^(?P<student_pk>[\d]+)/edit$', views.UpdateStudentView.as_view(), name='student-update',),
+    url(r'^add/$', views.CreateStudentView.as_view(), name='student-create',),
+    url(r'^(?P<student_pk>[\d]+)/delete$', views.DeleteStudentView.as_view(), name='student-delete',),
 ]
