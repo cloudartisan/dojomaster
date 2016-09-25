dc2fda7839b42264aadde8bc4b0c5b87983d3439
cloudartisan <david@cloudartisan.com>
2016-09-25 23:30:20 +1000

    Clean up

--- apps/clubs/models.py
+++ apps/clubs/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 from organizations.models import Organization
 
