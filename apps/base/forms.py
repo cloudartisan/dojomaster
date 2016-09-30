5cb745d2bb5b058fe745300dd8437395fba2085a
cloudartisan <david@cloudartisan.com>
2016-09-30 22:11:05 +1000

    Moved to users app

--- apps/base/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-
-
-class NameForm(forms.ModelForm):
-
-    class Meta(object):
-        model = User
-        fields = ('first_name', 'last_name')
