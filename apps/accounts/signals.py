a05deaa67cdeed6ecc03aefdfd1b5fac971124d7
cloudartisan <david@cloudartisan.com>
2016-10-01 20:46:04 +1000

    Documentation

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -6,6 +6,10 @@ from .models import UserAccount, UserAccountProfile
 
 @receiver(post_save, dispatch_uid="create_profile", sender=UserAccount)
 def create_profile(sender, **kwargs):
+    """
+    Receives the post_save signal from UserAccount. If a UserAccount has been
+    created, create and save the UserAccountProfile linked to it.
+    """
     user = kwargs["instance"]
     if kwargs["created"]:
         user_profile = UserAccountProfile(user=user)
