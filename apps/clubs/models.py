5be1215071fd342e3de0880e99fdfae98b50d501
cloudartisan <david@cloudartisan.com>
2016-10-02 10:28:18 +1100

    Adds date_created and date_updated

--- apps/clubs/models.py
+++ apps/clubs/models.py
@@ -1,12 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext as _
 from phonenumber_field.modelfields import PhoneNumberField
 from organizations.models import Organization
 
 
 class Club(Organization):
-    updated = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(
+        verbose_name=_('Date Created'),
+        auto_now_add=True
+    )
+    date_updated = models.DateTimeField(
+        verbose_name=_('Date Updated'),
+        auto_now=True
+    )
     url = models.URLField(max_length=200, blank=True)
     phone = PhoneNumberField(blank=True)
     mobile = PhoneNumberField(blank=True)
