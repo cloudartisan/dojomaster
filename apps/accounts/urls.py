5a4aaa33e1473c3b3ef88ceeb208db5417b61ce3
cloudartisan <david@cloudartisan.com>
2016-10-25 22:06:22 +1100

    Explicitly requires login for user profile change

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from .views import UserProfileChange
 
 
 urlpatterns = [
-    url(r'^profile/$', UserProfileChange.as_view(), name='user_profile_change'),
+    url(r'^profile/$', login_required(UserProfileChange.as_view()), name='user_profile_change'),
 ]
